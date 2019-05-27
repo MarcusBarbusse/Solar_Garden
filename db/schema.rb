0e8a0c9ef01b388c6f444a595501170ee1f8cc4b
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-05-27 17:55:47 +0200

    Adding booking model (#4)
    * migration done
    
    * added booking model and booking table to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_152106) do
+ActiveRecord::Schema.define(version: 2019_05_27_155040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "garden_id"
+    t.bigint "user_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["garden_id"], name: "index_bookings_on_garden_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "gardens", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "address"
+    t.string "city"
+    t.integer "postal_code"
+    t.string "country"
+    t.integer "square_meters"
+    t.integer "price"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +58,6 @@ ActiveRecord::Schema.define(version: 2019_05_27_152106) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "gardens"
+  add_foreign_key "bookings", "users"
 end
