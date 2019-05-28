244518dadfcc93dba18abe82368ddbe0afa92845
louis depraetere <louisdepraetere@hotmail.com>
2019-05-28 11:47:54 +0200

    changed the postal code into a string in the gardens and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_163204) do
+ActiveRecord::Schema.define(version: 2019_05_28_091435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_163204) do
     t.text "description"
     t.string "address"
     t.string "city"
-    t.integer "postal_code"
+    t.string "postal_code"
     t.string "country"
     t.integer "square_meters"
     t.integer "price"
