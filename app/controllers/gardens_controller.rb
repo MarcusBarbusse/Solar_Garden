dc0a7e60d7b76729dc32381e080174628867b524
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-05-28 17:58:21 +0200

    pundit nearly finished

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,10 +1,12 @@
 class GardensController < ApplicationController
+  before_action :find_garden, only: [:show, :edit, :update, :destroy]
   def index
-    @gardens = Garden.all
+    @gardens = policy_scope(Garden)
   end
 
   def new
     @garden = Garden.new
+    authorize @garden
   end
 
   def show
@@ -13,22 +15,39 @@ class GardensController < ApplicationController
 
   def create
     @garden = Garden.new(garden_params)
+    authorize @garden
     if @garden.save
       redirect_to gardens_path
     else
       render :new
     end
   end
+ def edit
+ end
+
+  def update
+    @garden.update(garden_params)
+    if @toy.save!
+      redirect_to garden_path(@garden)
+    else render :new
+    end
+  end
 
   def destroy
     @garden = Garden.find(params[:id])
     @garden.destroy
-    redirect_to root_path
+    redirect_to gardens_path
+
   end
 
   private
 
   def garden_params
     params.require(:garden).permit(:title, :description, :address, :city, :postal_code, :country, :square_meters, :price, :photo)
+  end
+
+  def find_garden
+    @garden = Garden.find(params[:id])
+    authorize @garden
   end
 end
