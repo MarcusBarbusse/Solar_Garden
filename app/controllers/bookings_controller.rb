b1fbe373d22d2b5c84a1950611eb59a2fd790cc1
louis depraetere <louisdepraetere@hotmail.com>
2019-05-29 11:45:40 +0200

    made the booking form work

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,7 +9,6 @@ class BookingsController < ApplicationController
   end
 
   def create
-
   @start_date = Date.civil(booking_params[:"start_date(1i)"].to_i,
                          booking_params[:"start_date(2i)"].to_i,
                          booking_params[:"start_date(3i)"].to_i)
@@ -24,8 +23,10 @@ class BookingsController < ApplicationController
     @garden = Garden.find(params[:garden_id])
     @booking = Booking.new(start_date: @start_date, end_date: @end_date)
     @booking.garden = @garden
-    @booking.user = User.last
+    @booking.user = User.find(7)
+
     if @booking.save
+
       redirect_to root_path
     else
       render :new
