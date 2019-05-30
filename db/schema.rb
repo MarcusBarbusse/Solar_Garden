327e3d4f27f9137f8ddd8a4ce2cc66d2c7370f63
Isabelle Polle <isabelle.polle@gmail.com>
2019-05-30 16:33:49 +0200

    front done

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_05_30_102016) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +36,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_102016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-
     t.string "category"
-
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_gardens_on_user_id"
