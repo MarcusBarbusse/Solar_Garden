3e3497e19fcf18f397a2e7a6da7ad3b406f51ff8
Henry Buisseret <henrybuisseret@gmail.com>
2019-05-30 16:09:01 +0200

    search sq still not good

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -6,8 +6,9 @@ class GardensController < ApplicationController
     if params[:search_query].present?
       sql_query = " \
       gardens.address @@ :query \
+      OR gardens.category @@ :query
       "
-      @gardens = policy_scope(Garden.where(category: params[:category]).where(sql_query, query: "%#{params[:search_query]}%"))
+      @gardens = policy_scope(Garden.where(sql_query, query: "%#{params[:search_query]}%").where(category: params[:category]))
     else
       @gardens = policy_scope(Garden)
     end
